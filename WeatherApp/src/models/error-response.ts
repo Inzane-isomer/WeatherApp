c2641a5ecda6d50fa6345f3d997257b5853930ce
Zane <zane@isomerindustries.co.za>
2025-03-26 15:18:54 +0200

    Added error messages/objects and error handling for weather api endpoint

---
+++ WeatherApp/src/models/error-response.ts
@@ -0,0 +1,28 @@
+import { MESSAGE_101, MESSAGE_601, MESSAGE_GENERAL } from "../enums/error-messages"
+
+export class ErrorResponse {
+    code: number;
+    message: string;
+    friendlyMessage: string;
+
+    constructor(code: number, info: string,) {
+        this.code = code;
+        this.message = info;
+        this.friendlyMessage = this.setErrorMessage(code);
+    }
+
+    setErrorMessage(code : number): string {
+        switch (code) {
+            case 101:
+                return MESSAGE_101;    
+            break;
+            case 601 :
+                return MESSAGE_601;
+            break;
+            default:
+                return MESSAGE_GENERAL;
+            break;
+        }
+    }
+
+}
