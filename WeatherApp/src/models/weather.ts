4e3693aecfb7c412c110c70c3affd13c168ed651
Zane <zane@isomerindustries.co.za>
2025-03-30 15:40:05 +0200

    Updated weather model to be more usable, weather service polish.

--- WeatherApp/src/models/weather.ts
+++ WeatherApp/src/models/weather.ts
@@ -5,12 +5,38 @@ export class Weather {
     historicalWeather?: CurrentWeather[];
     forecastWeather?: CurrentWeather[];
 
-    constructor(location: Location, currentWeather: CurrentWeather, historicalWeather?: CurrentWeather[], forecastWeather?: CurrentWeather[],) {
-        this.location = location,
-        this.currentWeather = currentWeather,
-        this.requestTime = new Date(),
-        this.historicalWeather = historicalWeather;
-        this.forecastWeather = forecastWeather;
+    constructor({location, currentWeather, historicalWeather, forecastWeather}: {location: object, currentWeather: object, historicalWeather?: object, forecastWeather?: object}) {
+        this.location = this.toLocation(location);
+        this.currentWeather = this.toCurrentWeather(currentWeather);
+        this.requestTime = new Date();
+        this.historicalWeather =  historicalWeather ? this.toWeatherArray(historicalWeather) : undefined;
+        this.forecastWeather = forecastWeather ? this.toWeatherArray(forecastWeather) : undefined;
+    }
+
+
+    toLocation(locationResponse : any): Location {
+        return {
+            country: locationResponse.country,
+            province: locationResponse.region,
+            city: locationResponse.name,
+        }
+    }
+
+    toCurrentWeather(weatherResponse : any): CurrentWeather {
+        return {
+            temperature: weatherResponse.temperature,
+            type: weatherResponse.weather_descriptions[0],
+            weatherIcon: weatherResponse.weather_icons[0],
+            windSpeed: weatherResponse.wind_speed,
+            precipitation: weatherResponse.precip,
+            pressure: weatherResponse.pressure,
+        }
+    }
+
+    toWeatherArray(dataObject: object): CurrentWeather[] {
+        return Object.values(dataObject).map((item : any)  => (
+            this.toCurrentWeather(item)
+        ));
     }
 }
 
@@ -27,6 +53,5 @@ export interface CurrentWeather {
     windSpeed: number,
     precipitation: number,
     pressure: number,
-
 }
 
