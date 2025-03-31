584462c6c20a2087841fd128052ea3fecdb1121f
Zane <zane@isomerindustries.co.za>
2025-03-31 12:45:06 +0200

    Add enums for weather icons and install library for weather animations

---
+++ WeatherApp/src/enums/weather-enums.ts
@@ -0,0 +1,7 @@
+import { BLUE, GREEN, GREY, YELLOW } from "./colors";
+
+export const DAYS = ['SUN','MON','TUE','WED','THU','FRI','SAT'];
+
+export const WEATHER_ICONS :Object = {'Sunny':'CLEAR_DAY', 'Partly Cloudy': 'PARTLY_CLOUDY_DAY', 'Overcast':'CLOUDY', 'Rainy': 'RAIN','Windy' : 'WIND','Fog':'FOG'}
+
+export const WEATHER_ICON_COLOR :Object = {'CLEAR_DAY' : YELLOW, 'PARTLY_CLOUDY_DAY' : GREY, 'CLOUDY': GREY, 'RAIN' : BLUE ,'WIND': GREEN,'FOG' : GREEN}
--- WeatherApp/src/models/weather.ts
+++ WeatherApp/src/models/weather.ts
@@ -1,3 +1,5 @@
+import { DAYS, WEATHER_ICON_COLOR, WEATHER_ICONS } from "../enums/weather-enums";
+
 export class Weather {
     location: Location;
     currentWeather: CurrentWeather;
@@ -23,10 +25,28 @@ export class Weather {
     }
 
     toCurrentWeather(weatherResponse : any): CurrentWeather {
+        let weatherIcons : string[] = [];
+        let weatherColor : string[] = [];
+
+        Object.entries(WEATHER_ICONS).forEach(([key, value]) => {
+            if(key=== weatherResponse.weather_descriptions[0]) {
+                weatherIcons.push(value);
+            }
+        })
+
+        Object.entries(WEATHER_ICON_COLOR).forEach(([key, value]) => {
+            if(key=== weatherIcons[0]) {
+                weatherColor.push(value);
+            }
+        })
+        
         return {
             temperature: weatherResponse.temperature,
+            date: weatherResponse.date,
+            day: DAYS[new Date(weatherResponse.date).getDay()],
             type: weatherResponse.weather_descriptions[0],
-            weatherIcon: weatherResponse.weather_icons[0],
+            weatherIcon: weatherIcons[0] ? weatherIcons[0] : weatherResponse.weather_icons[0],
+            iconColor: weatherColor[0] ? weatherColor[0] : 'black',
             windSpeed: weatherResponse.wind_speed,
             precipitation: weatherResponse.precip,
             pressure: weatherResponse.pressure,
@@ -48,8 +68,11 @@ export interface Location {
 
 export interface CurrentWeather {
     temperature: number,
+    date?: Date,
+    day?: string,
     type: string,
     weatherIcon: string,
+    iconColor?: string,
     windSpeed: number,
     precipitation: number,
     pressure: number,
