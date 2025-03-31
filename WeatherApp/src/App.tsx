bf85a00e6899c2a920d38b2cd0e9d00e0d50a3a9
Zane <zane@isomerindustries.co.za>
2025-03-31 12:47:15 +0200

    Complete styling of root app page and add in state variables

--- WeatherApp/src/App.tsx
+++ WeatherApp/src/App.tsx
@@ -1,23 +1,15 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
-import { getDefaultWeatherData } from './services/weather-service'
-import { Weather } from './models/weather'
+import { getDefaultWeatherData, getForecastWeatherData, getHistoricalWeatherData } from './services/weather-service'
+import { CurrentWeather, Location, Weather } from './models/weather'
 import { ErrorResponse } from './models/error-response'
-import { BORDER_COLOR, PRIMARY_BG_COLOR, SECONDARY_BG_COLOR } from './enums/colors'
+import { BORDER_COLOR, PRIMARY_BG_COLOR, RED, SECONDARY_BG_COLOR } from './enums/colors'
+import { TypographyParagraph } from './components/Typography'
+import UpperCardArea from './components/UpperCardArea'
 import styled from 'styled-components'
+import LowerCardArea from './components/LowerCardArea'
+import Skeleton from 'react-loading-skeleton'
 
-function App() {
-  const [count, setCount] = useState(0)
-  const [weatherRes, setWeatherRes] = useState<Weather | null>();
-  const [errorRes, setErrorRes] = useState<ErrorResponse | null>();
-
-  async function handleClick() {
-    const res = await getDefaultWeatherData();
-    if(res instanceof Weather) {
-      return setWeatherRes(res)  
-    }
-    return setErrorRes(res);
-  }
 
   const StyledOuterCard = styled.div`
   display: flex;
@@ -28,6 +20,8 @@ function App() {
 `;
 
 const StyledInnerCard = styled.div`
+  display: flex;
+  flex-flow: column;
   width: 700px;
   height: 450px;
   background-color: ${PRIMARY_BG_COLOR};
@@ -38,15 +32,82 @@ const StyledInnerCard = styled.div`
   align-self: center;
 `;
 
+const StyledTitle = styled.div`
+  flex: 0.5;
+  text-align: center;
+  line-height: 1;
+  font-weight: 600;
+`;
+
+const StyledUpperCardArea = styled.div`
+  width: 100%;
+  flex: 2;
+`;
+
+const StyledLowerCardArea = styled.div`
+  width: 100%;
+  flex: 2;
+`;
+
+export default function App() {
+  const [loading, setLoading] = useState(false);
+  const [selectedWeather, setSelectedWeather] = useState<CurrentWeather>();
+  const [currentLocation, setCurrentLocation] = useState<Location>();
+  const [errorRes, setErrorRes] = useState<ErrorResponse | null>();
+  const [errorMessage, setErrorMessage] = useState<string>();
+  const [weeklyWeather, setWeeklyWeather] = useState<CurrentWeather[]>();
+  
+  async function handleApiReq() {
+    setLoading(true);
+    const forecastRes = await getForecastWeatherData();
+    let forecastArr : [] = [];
+    let historicalArr = new Array();
+    if(forecastRes instanceof Weather) {
+      forecastArr = [...forecastRes.forecastWeather as []];
+      setSelectedWeather(forecastRes.currentWeather)  
+      setCurrentLocation(forecastRes.location);
+    }else {
+      setErrorRes(forecastRes);
+      setErrorMessage(forecastRes.friendlyMessage);
+    }
+    
+    const historicalRes = await getHistoricalWeatherData();
+    if(historicalRes instanceof Weather) {
+      historicalArr = [...historicalRes.historicalWeather as []];
+      historicalArr.push(historicalRes.currentWeather);
+      setCurrentLocation(historicalRes.location);
+
+    } else {
+      setErrorRes(historicalRes);
+    }
+    setWeeklyWeather([...historicalArr, ...forecastArr]);
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    handleApiReq();
+  }, []);
+
+  async function handleClick(weatherDetails : any) {
+    setSelectedWeather(weatherDetails)
+  }
+  
   return (
-    <>
     <StyledOuterCard>
       <StyledInnerCard>
-      
+        <StyledTitle>
+          <TypographyParagraph textSize={20} textColor={errorRes ? RED : "" }>
+            {loading ? "Loading..." : (!errorRes ? `${currentLocation?.city}, ${currentLocation?.province}, ${currentLocation?.country}` : "")}
+            {errorRes ? errorRes.friendlyMessage : ""}
+          </TypographyParagraph>
+        </StyledTitle>
+        <StyledUpperCardArea >
+          <UpperCardArea error={errorMessage!} handleEvent={handleClick} weather={selectedWeather} loading={loading}/>
+        </StyledUpperCardArea>
+        <StyledLowerCardArea>
+          <LowerCardArea  weeklyWeather={weeklyWeather!} handleEvent={handleClick} loading={false} />
+        </StyledLowerCardArea>
       </StyledInnerCard>
     </StyledOuterCard>
-    </>
   )
 }
-
-export default App
